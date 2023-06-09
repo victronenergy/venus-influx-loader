05f29f91fba97bb51bff6ce1e600cd45cc98fc0e
Martin Man <mman@martinman.net>
2023-06-09 19:52:25 +0200

    Move Venus Grafana Dashboards to victronenergy/venus-grafana-dashboards

--- docker-grafana/build-dev-image.sh
+++
@@ -1,9 +0,0 @@
-VER=develop
-
-REPO=victronenergy
-TARGET=venus-docker-grafana
-BUILD_OPTS="--no-cache --pull --progress=plain"
-
-TAG="$REPO/$TARGET:${VER}"
-
-docker build ${BUILD_OPTS} -t ${TAG} .
