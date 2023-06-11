7e9041291a99991a29008dec108e15b8f0fce724
Martin Man <mman@martinman.net>
2023-06-11 11:44:45 +0200

    Add `run-influxdb.sh` script to spin up dev instance of InfluxDB

---
+++ docker/run-influxdb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+TAG="influxdb:1.8"
+NETWORK="vil"
+
+INFLUXDB_ADMIN_USER=s3cr4t
+INFLUXDB_ADMIN_PASSWORD=s3cr4t
+
+docker network inspect $NETWORK 1>/dev/null 2>/dev/null || docker network create $NETWORK
+
+docker run -it -p 8086:8086 --network $NETWORK \
+    -e INFLUXDB_HTTP_LOG_ENABLED=false \
+    -e INFLUXDB_HTTP_AUTH_ENABLED=true \
+    -e INFLUXDB_ADMIN_USER=$INFLUXDB_ADMIN_USER \
+    -e INFLUXDB_ADMIN_PASSWORD=$INFLUXDB_ADMIN_PASSWORD \
+    ${TAG}
