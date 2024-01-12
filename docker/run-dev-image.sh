21cafda4990d6ef336c69b136a2584cf3b41bed4
Martin Man <mman@martinman.net>
2024-01-12 13:11:47 +0100

    fix: add proper shebang to dev scripts, make run-dev-image.sh work under Windows/Git-bash

--- docker/run-dev-image.sh
+++ docker/run-dev-image.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 VER=develop
 
@@ -16,8 +16,8 @@ VIL_INFLUXDB_PASSWORD=s3cr4t
 docker network inspect $NETWORK 1>/dev/null 2>/dev/null || docker network create $NETWORK
 
 docker run -p 8088:8088 --network $NETWORK \
-    -e VIL_INFLUXDB_URL=$VIL_INFLUXDB_URL \
-    -e VIL_INFLUXDB_USERNAME=$VIL_INFLUXDB_USERNAME \
-    -e VIL_INFLUXDB_PASSWORD=$VIL_INFLUXDB_PASSWORD \
-    -v $PWD/../config:/config \
-    ${TAG}
+    -e "VIL_INFLUXDB_URL=$VIL_INFLUXDB_URL" \
+    -e "VIL_INFLUXDB_USERNAME=$VIL_INFLUXDB_USERNAME" \
+    -e "VIL_INFLUXDB_PASSWORD=$VIL_INFLUXDB_PASSWORD" \
+    -v "$PWD/../config:/config" \
+    "${TAG}"
