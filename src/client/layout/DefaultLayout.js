230179a7dcf9597b30f16b1888eaa62d71a5bdd8
Martin Man <mman@martinman.net>
2024-05-17 18:57:48 +0200

    fix(scss): sync styling with latest version of coreui

--- src/client/layout/DefaultLayout.js
+++ src/client/layout/DefaultLayout.js
@@ -5,9 +5,9 @@ const DefaultLayout = () => {
   return (
     <div>
       <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+      <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
-        <div className="body flex-grow-1 px-3">
+        <div className="body flex-grow-1">
           <AppContent />
         </div>
         <AppFooter />
