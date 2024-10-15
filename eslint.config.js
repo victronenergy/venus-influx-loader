2c81c0e0e97533a83a7392aba8644b76432ac52b
Martin Man <mman@martinman.net>
2024-10-15 20:57:44 +0200

    chore: switch to @typescript-eslint/parser

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,30 @@
-import { configs as jsConfigs } from "@eslint/js"
-import babelParser from "@babel/eslint-parser"
-import reactPlugin, { configs as reactConfigs } from "eslint-plugin-react"
+import js from "@eslint/js"
+import tsParser from "@typescript-eslint/parser"
+import tsPlugin from "typescript-eslint"
+import reactPlugin from "eslint-plugin-react"
 import prettierPluginRecommended from "eslint-plugin-prettier/recommended"
-import { node, browser } from "globals"
+import globals from "globals"
 import confusingBrowserGlobals from "confusing-browser-globals"
 
 export default [
-  jsConfigs.recommended,
+  js.configs.recommended,
   prettierPluginRecommended,
   {
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     plugins: {
       react: reactPlugin,
+      typescript: tsPlugin,
     },
     rules: {
-      ...reactConfigs.recommended.rules,
-      ...reactConfigs["jsx-runtime"].rules,
-      "no-unused-vars": [
-        "error",
-        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-      ],
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...tsPlugin.configs.recommended.rules,
+      "no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
     },
     languageOptions: {
-      parser: babelParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
+      parser: tsParser,
       globals: {
-        ...node,
+        ...globals.node,
       },
     },
     settings: {
@@ -39,8 +37,8 @@ export default [
     files: ["src/client/**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     languageOptions: {
       globals: {
-        ...browser,
-        ...reactConfigs.settings.react.globals,
+        ...globals.browser,
+        ...reactPlugin.configs.recommended.globals,
         VENUS_INFLUX_LOADER_ADMIN_API_PORT: true,
         VENUS_INFLUX_LOADER_BUILD_VERSION: true,
       },
@@ -53,7 +51,7 @@ export default [
     files: ["src/server/**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     languageOptions: {
       globals: {
-        ...node,
+        ...globals.node,
       },
     },
   },
