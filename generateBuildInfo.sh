d6977ab40c9dccaf4dd5e59f3140bb8f5e49699e
Martin Man <mman@martinman.net>
2024-10-15 22:42:18 +0200

    fix: generateBuildInfo.sh should produce .cjs

--- generateBuildInfo.sh
+++ generateBuildInfo.sh
@@ -3,4 +3,4 @@
 BUILD_VERSION=${BUILD_VERSION:=$(git describe --tags)}
 
 mkdir -p ./dist
-echo "// @ts-check\nmodule.exports.buildVersion=\"${BUILD_VERSION}\"" > ./dist/buildInfo.js
+echo "// @ts-check\nmodule.exports.buildVersion=\"${BUILD_VERSION}\"" > ./dist/buildInfo.cjs
