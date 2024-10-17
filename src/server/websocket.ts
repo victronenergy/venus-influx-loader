429d077288fe6ee29ee2cfba11bc17cc1f7cb0e7
Martin Man <mman@martinman.net>
2024-10-17 16:45:41 +0200

    feat: .js -> websocket.ts

---
+++ src/server/websocket.ts
@@ -0,0 +1,79 @@
+import Primus from "primus"
+import { Server } from "./server"
+import { Logger } from "winston"
+
+export class WebSocketChannel {
+  server: Server
+  logger: Logger
+  private primus!: Primus
+
+  constructor(server: Server) {
+    this.server = server
+    this.logger = server.getLogger("ws")
+  }
+
+  start() {
+    this.logger.debug("Starting Primus/WS interface...")
+
+    const primusOptions = {
+      transformer: "websockets",
+      pingInterval: false,
+      pathname: "/stream",
+    }
+
+    // @ts-ignore
+    this.primus = new Primus(this.server.httpServer, primusOptions)
+    this.primus.on("connection", (spark) => {
+      this.logger.debug(`${spark.id} connected`)
+
+      spark.on("end", function () {})
+
+      // @ts-ignore
+      spark.onDisconnects = []
+
+      const onServerEvent = (event: any) => {
+        spark.write(event)
+      }
+
+      this.server.app.on("serverevent", onServerEvent)
+
+      // @ts-ignore
+      spark.onDisconnects.push(() => {
+        this.server.app.removeListener("serverevent", onServerEvent)
+      })
+
+      Object.entries(this.server.lastServerEvents).forEach(([type, event]) => {
+        if (type !== "LOG") {
+          spark.write(event)
+        }
+      })
+      this.server.logEntries.forEach((entry) => {
+        spark.write({
+          type: "LOG",
+          data: entry,
+        })
+      })
+    })
+
+    this.primus.on("disconnection", (spark) => {
+      this.logger.debug(`${spark.id} disconnected`)
+
+      // @ts-ignore
+      spark.onDisconnects.forEach((f: () => void) => f())
+    })
+  }
+
+  stop() {
+    this.logger.debug("Stopping Primus/WS interface...")
+    this.primus.destroy(
+      {
+        close: false,
+        timeout: 500,
+        reconnect: false,
+      },
+      () => {
+        this.logger.debug("Primus/WS interface stopped.")
+      },
+    )
+  }
+}
