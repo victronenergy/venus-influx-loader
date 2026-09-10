45488f88ac1dab82af043e6fd72741c5f8a992b3
Martin Man <mman@martinman.net>
2026-09-10 11:41:28 +0200

    refactor: extract CLI option parsing into server/cli.ts

--- src/bin/venus-influx-loader.ts
+++ src/bin/venus-influx-loader.ts
@@ -1,70 +1,27 @@
 #!/usr/bin/env node
 
-import { program } from "commander"
 // @ts-expect-error
 import buildInfo from "../buildInfo.cjs"
+import ms from "ms"
 
 import { Server } from "../server/server"
-import ms, { StringValue } from "ms"
+import { parseCliOptions, programName } from "../server/cli"
 
-const autoExpiryDefault = "30d"
-
-program
-  .version(buildInfo.buildVersion)
-  .description("Monitor Venus devices and capture & store realtime data to serve Grafana")
-  .option("-c, --config-path <path>", "path to store config.json and secrets.json", "/config")
-  .option("-p, --port <port>", "http port used by Admin Web User Interface and Grafana JSON datasource", "8088")
-  .option(
-    "--grafana-url <url>",
-    "http link to Grafana",
-    "${window.location.protocol}//${window.location.hostname}:3000",
-  )
-  .option("--disable-admin-api", "disable Admin Web User Interface and /admin-api/ endpoint")
-  .option(
-    "--disable-admin-api-auth",
-    "disable password protection for Admin Web User Interface and /admin-api/ endpoint",
-  )
-  .option("--disable-grafana-api", "disable Grafana JSON datasource /grafana-api/ endpoint")
-  .option("--enable-discovery-api", "enable venus-upnp-browser /discovery-api/ endpoint")
-  .option("--enable-auto-expiry [duration]", "enable automatic expiry of data collection", autoExpiryDefault)
-  .option("--hide-settings-influxdb")
-  .option("--hide-settings-security")
-  .option("--hide-settings-venus-discovery")
-  .option("--hide-settings-venus-manual")
-  .option("--hide-settings-venus-vrm")
-
-program.parse()
-const options = program.opts()
+const options = parseCliOptions(process.argv.slice(2), buildInfo.buildVersion)
 
 function log(message: string) {
-  console.log(`${program.name()}: ${message}`)
+  console.log(`${programName}: ${message}`)
 }
 
-const discoveryApi = options.enableDiscoveryApi ? "/discovery-api/" : undefined
-const adminApi = options.disableAdminApi ? undefined : "/admin-api/"
-const adminApiAuthEnabled = options.disableAdminApiAuth ? false : true
-const grafanaApi = options.disableGrafanaApi ? undefined : "/grafana-api/"
-const port = options.port
-const grafanaUrl = options.grafanaUrl
-
-// extract autoExpiryDuration, but only when the option was used
-// to workaround commander limitation where default option value
-// is always present even if the option was not used at all
-let autoExpiryDuration: number
-if (process.argv.includes("--enable-auto-expiry")) {
-  autoExpiryDuration =
-    options.enableAutoExpiry === true ? ms(autoExpiryDefault) : ms(options.enableAutoExpiry as StringValue)
-} else {
-  autoExpiryDuration = 0 // disabled
-}
+const autoExpiryDuration = options.uiSettings.showAutomaticExpirySettings ?? 0
 
 log("Use --help to learn how to use this program")
 log(`Config Path: ${options.configPath}`)
-log(`Discovery API: ${discoveryApi || "disabled"}`)
-log(`Admin API: ${adminApi || "disabled"}`)
-log(`Grafana JSON Datasource API: ${grafanaApi || "disabled"}`)
-log(`API Port: ${adminApi || grafanaApi ? port : "disabled"}`)
-log(`Grafana URL: ${grafanaUrl}`)
+log(`Discovery API: ${options.discoveryApiEndpoint || "disabled"}`)
+log(`Admin API: ${options.adminApiEndpoint || "disabled"}`)
+log(`Grafana JSON Datasource API: ${options.grafanaApiEndpoint || "disabled"}`)
+log(`API Port: ${options.adminApiEndpoint || options.grafanaApiEndpoint ? options.port : "disabled"}`)
+log(`Grafana URL: ${options.uiSettings.grafanaUrl}`)
 log(`Automatic Data Collection Expiry: ${autoExpiryDuration > 0 ? ms(autoExpiryDuration, { long: true }) : "disabled"}`)
 
 // exit on `docker stop` or Ctrl-C
@@ -76,23 +33,7 @@ signals.forEach((signal: string) => {
   })
 })
 
-const server = new Server({
-  configPath: options.configPath,
-  port: Number(port),
-  discoveryApiEndpoint: discoveryApi,
-  adminApiEndpoint: adminApi,
-  adminApiEndpointAuthEnabled: adminApiAuthEnabled,
-  grafanaApiEndpoint: grafanaApi,
-  uiSettings: {
-    grafanaUrl: grafanaUrl,
-    showEditDiscoverySettings: !options.hideSettingsVenusDiscovery,
-    showEditVRMSettings: !options.hideSettingsVenusVRM,
-    showEditManualSettings: !options.hideSettingsVenusManual,
-    showEditSecuritySettings: !options.hideSettingsSecurity && adminApiAuthEnabled,
-    showEditInfluxDBSettings: !options.hideSettingsInfluxdb,
-    showAutomaticExpirySettings: autoExpiryDuration > 0 ? autoExpiryDuration : undefined,
-  },
-})
+const server = new Server(options)
 
 // start server
 server.start()
---
+++ src/server/cli.ts
@@ -0,0 +1,70 @@
+import { Command } from "commander"
+import ms, { StringValue } from "ms"
+import type { ServerOptions } from "./server"
+
+export const programName = "venus-influx-loader"
+export const autoExpiryDefault = "30d"
+
+export function createProgram(version: string): Command {
+  return new Command()
+    .name(programName)
+    .version(version)
+    .description("Monitor Venus devices and capture & store realtime data to serve Grafana")
+    .option("-c, --config-path <path>", "path to store config.json and secrets.json", "/config")
+    .option("-p, --port <port>", "http port used by Admin Web User Interface and Grafana JSON datasource", "8088")
+    .option(
+      "--grafana-url <url>",
+      "http link to Grafana",
+      "${window.location.protocol}//${window.location.hostname}:3000",
+    )
+    .option("--disable-admin-api", "disable Admin Web User Interface and /admin-api/ endpoint")
+    .option(
+      "--disable-admin-api-auth",
+      "disable password protection for Admin Web User Interface and /admin-api/ endpoint",
+    )
+    .option("--disable-grafana-api", "disable Grafana JSON datasource /grafana-api/ endpoint")
+    .option("--enable-discovery-api", "enable venus-upnp-browser /discovery-api/ endpoint")
+    .option("--enable-auto-expiry [duration]", "enable automatic expiry of data collection", autoExpiryDefault)
+    .option("--hide-settings-influxdb")
+    .option("--hide-settings-security")
+    .option("--hide-settings-venus-discovery")
+    .option("--hide-settings-venus-manual")
+    .option("--hide-settings-venus-vrm")
+}
+
+// argv: user arguments only (i.e. process.argv.slice(2))
+export function parseCliOptions(argv: string[], version: string): ServerOptions {
+  const program = createProgram(version)
+  program.parse(argv, { from: "user" })
+  const options = program.opts()
+
+  const adminApiAuthEnabled = options.disableAdminApiAuth ? false : true
+
+  // only honour auto expiry when the option was actually given on the command line,
+  // commander always reports the default value otherwise
+  let autoExpiryDuration: number
+  if (program.getOptionValueSource("enableAutoExpiry") === "cli") {
+    autoExpiryDuration =
+      options.enableAutoExpiry === true ? ms(autoExpiryDefault) : ms(options.enableAutoExpiry as StringValue)
+  } else {
+    autoExpiryDuration = 0 // disabled
+  }
+
+  return {
+    configPath: options.configPath,
+    port: Number(options.port),
+    discoveryApiEndpoint: options.enableDiscoveryApi ? "/discovery-api/" : undefined,
+    adminApiEndpoint: options.disableAdminApi ? undefined : "/admin-api/",
+    adminApiEndpointAuthEnabled: adminApiAuthEnabled,
+    grafanaApiEndpoint: options.disableGrafanaApi ? undefined : "/grafana-api/",
+    uiSettings: {
+      grafanaUrl: options.grafanaUrl,
+      showEditDiscoverySettings: !options.hideSettingsVenusDiscovery,
+      showEditVRMSettings: !options.hideSettingsVenusVRM,
+      showEditManualSettings: !options.hideSettingsVenusManual,
+      showEditSecuritySettings: !options.hideSettingsSecurity && adminApiAuthEnabled,
+      showEditInfluxDBSettings: !options.hideSettingsInfluxdb,
+      showAutomaticExpirySettings: autoExpiryDuration > 0 ? autoExpiryDuration : undefined,
+    },
+  }
+}
